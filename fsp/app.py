d2a945dc9b6b69403252e6cacb8d380503dc814e
Nikita Rydanov <wrongwayboyy@gmail.com>
2023-04-27 20:36:24 +0300

    Возможно работающий прототип

--- fsp/app.py
+++ fsp/app.py
@@ -1,17 +1,17 @@
 import os
 from flask import Flask, request, session, abort
+from flask_cors import CORS
 
 from db.utils import init_connection
-from utils import is_none_or_empty, hash_password
-from service import UserService
+from utils import is_none_or_empty, hash_password, make_default_asserts
+from service import ServiceManager, UserService, EventService
 
-from sqlalchemy import MetaData
-
-from entity import User
+from entity import User, Event
 
 import bcrypt
 
 app = Flask(__name__)
+CORS(app)
 
 secret = os.environ.get('SECRET')
 if secret is None:
@@ -20,7 +20,7 @@ app.secret_key = secret
 
 engine, sess = init_connection('postgresql+psycopg2://postgres:postgres@localhost/postgres')
 
-user_service = UserService(sess)
+services = ServiceManager()
 
 @app.post('/auth/login')
 def login():
@@ -33,11 +33,12 @@ def login():
     400 Bad Request, если логин неверный или пользователя с
     именем `login` не существует.
     """
-
-    # Content-Type не равен application/json
+    
+    user_service = ServiceManager.get(UserService)
+    
     body = request.json
-    if body is None: 
-        abort(400)
+    
+    make_default_asserts(body)
         
     password = body['password']
     email = body['email']
@@ -45,8 +46,7 @@ def login():
     # логин или пароль не предоставлены
     if is_none_or_empty(email) or is_none_or_empty(password):
         abort(400)
-
-    
+        
     user = user_service.get_by_login(email)
     # пользователя с таким именем не существует
     if user is None:
@@ -55,7 +55,6 @@ def login():
 
     hashed_password = user.password
     
-    # пароль неверный
     if not bcrypt.checkpw(password.encode('utf-8'), hashed_password):
         abort(400)
 
@@ -74,23 +73,17 @@ def register():
     При успешной регистрации ответ 201 Created.
     Если имя занято, возвращает 400 Bad Request.
     """
+    
+    user_service = ServiceManager(UserService)
 
     body = dict(request.json)
-    # Content-Type не равен application/json
-    if body is None: 
-        abort(400)
-
-    # username или пароль не предоставлены
+    make_default_asserts(body)
+    
     user: User = user_service.get_by_login(body['email'])
 
-    # # пользователя с таким именем существует
+    # Если пользователь уже существует
     if user is not None:
-        print('aboba')
-        abort(400)
-
-    if any([is_none_or_empty(v) for v in body.values()]):
-        print('aboba2')
-        abort(400)
+        abort(400, "Этот пользователь уже существует")
 
     body['password'] = hash_password(body['password'])
 
@@ -99,8 +92,37 @@ def register():
         ok = user_service.add(user)
     except Exception as e:
         print(e)
-        abort(400)
+        abort(400, e)
     
     ok = True
 
     return ('', 201) if ok else ('', 400)
+
+@app.post('/events/add')
+def add_event():
+    
+    event_service: EventService = ServiceManager.get(EventService)
+    
+    body = dict(request.json)
+    
+    try:
+        event = Event(**body)
+        response = event_service.add(Event(**body))
+    except Exception as e:
+        abort(400, e)
+        
+@app.post('/events/get')
+def add_event():
+    
+    event_service: EventService = ServiceManager.get(EventService)
+    
+    page = request.args.get('page')
+    per_page = request.args.get('per_page')
+    
+    body = dict(request.json)
+    
+    try:
+        event = Event(**body)
+        response = event_service.get(page, per_page)
+    except Exception as e:
+        abort(400, e)
