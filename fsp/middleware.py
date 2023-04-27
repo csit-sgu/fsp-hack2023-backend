77621bb31febb08d2603beafcdf464cc22e0919f
Nikita Rydanov <wrongwayboyy@gmail.com>
2023-04-27 21:48:17 +0300

    Исправления по патчу с Middleware и merge'у

---
+++ fsp/middleware.py
@@ -0,0 +1,17 @@
+from flask import Request, abort
+
+class CheckFields:
+
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        request = Request(environ)
+        if request.content_type.lower() == "application/json":
+            if request.json is None or any([d is None for d in dict(request.json).values()]):
+                abort(400, "Недостающие параметры запроса")
+        else:
+            abort(400, "Неверный тип запроса")
+            
+
+        return self.app(environ, start_response)
