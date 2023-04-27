18d0a76e2cd70c7039a1bd57f6947a049e86e484
kizzzil <89316640+kizzzil@users.noreply.github.com>
2023-04-27 14:00:15 +0400

    Адаптировал класс юзер (#5)

--- fsp/models.py
+++ fsp/models.py
@@ -1,18 +1,26 @@
 from dataclasses import dataclass
 from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import Table, String, Integer, Column, Text, DateTime, Boolean, Enum
+from datetime import datetime
+import enum
+
+class Claims(enum.Enum):
+    ATHLETE
+    REPRESENTATIVE 
+    ADMINISTRATOR
+    PARTNER
 
 Base = DeclarativeBase()
 
 @dataclass
 class User(Base):
-    guid : int
-    claims : list
-    date_reg : str
-    date_login : str
-    salt : bytes
-    hashed_password : bytes
-    athlete_FK : int
-    representative_FK : int
-    administator_FK : int
-    parthner_FK : int
-    email : str
+    uid = Column(Integer, primary_key=True, nullable=False)
+    claims = Column(Enum(Claims),nullable=False)
+    date_reg = Column(DateTime(), default=datetime.now, nullable=False)
+    date_login = Column(DateTime(), default=datetime.now, onupdate=datetime.now, nullable=False)
+    hashed_password = Column(String(255), nullable=False)
+    athlete_FK = Column(Integer, nullable=True)
+    representative_FK = Column(Integer, nullable=True)
+    administrator_FK = Column(Integer, nullable=True)
+    partner_FK = Column(Integer, nullable=True)
+    email: Column(String(255), nullable=False)
