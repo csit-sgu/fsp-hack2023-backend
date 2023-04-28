a3240cc2e8933f0a7fd2672070c5877aff0c65c3
Nikita Rydanov <wrongwayboyy@gmail.com>
2023-04-28 08:55:01 +0300

    И ещё...

--- fsp/token.py
+++ fsp/token.py
@@ -3,20 +3,11 @@ import os
 
 from datetime import datetime, timedelta
 from .db.models import Claim
-<<<<<<< HEAD
 from .settings import config
-=======
-
-_SECRET = os.environ.get("SECRET")
-if _SECRET is None:
-    raise RuntimeError("Please, set the SECRET environment variable")
-
->>>>>>> dd2bb88 (Проверенные эндпоинты + начало функционала работа с командами)
 
 class JWT:
     @staticmethod
     def create(claims: dict) -> str:
-<<<<<<< HEAD
         curr_time = datetime.now(tz=datetime.timezone.utc)
         delta = timedelta(seconds=config.token_expiration_time_sec)
 
@@ -29,9 +20,6 @@ class JWT:
             config.secret,
             algorithm="HS256"
         )
-=======
-        return jwt.encode(claims, _SECRET, algorithm="HS256")
->>>>>>> dd2bb88 (Проверенные эндпоинты + начало функционала работа с командами)
 
     @staticmethod
     def extract(token: str | None) -> dict | None:
@@ -39,11 +27,7 @@ class JWT:
             return None
 
         try:
-<<<<<<< HEAD
             return jwt.decode(token, config.secret, algorithms=["HS256"])
-=======
-            return jwt.decode(token, _SECRET, algorithms=["HS256"])
->>>>>>> dd2bb88 (Проверенные эндпоинты + начало функционала работа с командами)
         except jwt.InvalidTokenError as e:
             print(f"Not decoding the token: {e}")
             return None
